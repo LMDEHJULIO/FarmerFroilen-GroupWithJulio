3ba373e57bd9f3feec02eea626eaf8c2dfd536ba
tanzirtal <tanzirtalp@gmail.com>
2023-09-28 15:00:52 -0400

    test

--- src/main/java/org/FarmerFroilen/Tractor.java
+++ src/main/java/org/FarmerFroilen/Tractor.java
@@ -1,2 +1,14 @@
-package org.FarmerFroilen;public class Tractor {
+package org.FarmerFroilen;
+
+public class Tractor {
+    /**
+     * Tractor should have a name which is a String
+     * Tractor will harvest a crop object and not return a value
+     *
+     * String name
+     * @method1- harvest()
+     * @param- crop
+     * @return- VOID
+     */
+
 }
