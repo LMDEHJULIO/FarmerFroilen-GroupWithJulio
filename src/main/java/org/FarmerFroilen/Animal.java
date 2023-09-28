2982271001e244e5aca0f738be2c9a6fc85fbe05
Julio Rodriguez <juliodevelops@gmail.com>
2023-09-28 14:43:26 -0400

    Add animal class and sudocode

---
+++ src/main/java/org/FarmerFroilen/Animal.java
@@ -0,0 +1,24 @@
+package org.FarmerFroilen;
+
+// Animal should implement NoiseMaker and Eater
+//
+public class Animal {
+
+    /**
+     *
+     * @param sumToSay - string representing what will be appending and returned
+     * @return String - concatentation of sumToSay + "!"
+     */
+
+    // instance variables
+
+    // getters/setters for ivars
+
+    // Nullary Constructor
+    // Constructor including param/ivars
+
+
+//    public String saySum(String sumToSay){
+//        return sumToSay + "!";
+//    }
+}
