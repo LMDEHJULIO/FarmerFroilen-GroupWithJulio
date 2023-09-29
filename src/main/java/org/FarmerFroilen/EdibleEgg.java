58c9a5c37cce5d5fb6930d3e4c56797688d3539c
Julio Rodriguez <juliodevelops@gmail.com>
2023-09-29 10:59:10 -0400

    Refactor edible egg to include hatch/expirationDate and constructor

--- src/main/java/org/FarmerFroilen/EdibleEgg.java
+++ src/main/java/org/FarmerFroilen/EdibleEgg.java
@@ -1,7 +1,41 @@
 package org.FarmerFroilen;
+import java.time.LocalDate;
+
 
 public class EdibleEgg implements Edible{
     private String description="I am packed with protein!";
+
+    // W3 Date classes -  https://www.w3schools.com/java/java_date.asp
+
+    private final LocalDate hatchDate;
+    private final LocalDate expirationDate;
+
+    /**
+     *  Nullary EdibleEgg constructor - hatchDate set to current day - expiration date 30 days from the hatch date.
+     */
+
+    public EdibleEgg(){
+        this.hatchDate = LocalDate.now();
+        this.expirationDate = hatchDate.plusDays(30);
+    }
+
+    /**
+     * Gets the hatch date of the EdibleEgg.
+     * @return LocalDate the hatch date of the EdibleEgg
+     */
+
+    public LocalDate getHatchDate() {
+        return hatchDate;
+    }
+
+    /**
+     * Gets the expiration date of the EdibleEgg.
+     * @return LocalDate the expiration date of the EdibleEgg
+     */
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
 
     /**
      * No constructor for the EdibleEgg class
