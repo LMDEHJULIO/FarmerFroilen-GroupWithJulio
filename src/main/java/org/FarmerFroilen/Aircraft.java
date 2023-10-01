d3a6115b541c013a0bb4ae4c1d1477569e62ab83
SpaceTilmar <21tilmaro@gmail.com>
2023-10-01 12:27:11 -0400

    Updated CropDuster, Aircraft

--- src/main/java/org/FarmerFroilen/Aircraft.java
+++ src/main/java/org/FarmerFroilen/Aircraft.java
@@ -6,6 +6,7 @@ public abstract class Aircraft extends Vehicle {
         super(name);
     }
 
+
     public void fly(){
 
     }
--- src/main/java/org/FarmerFroilen/CropDuster.java
+++ src/main/java/org/FarmerFroilen/CropDuster.java
@@ -1,10 +1,18 @@
 package org.FarmerFroilen;
-
+import java.util.*;
 public class CropDuster extends Aircraft implements FarmVehicle {
     // test error
     public CropDuster(String name) {
         super(name);
     }
+/*
+Iterate through the cropRow arrayList and check if each crop hasBeenFertilized, if not then fertilize the crop.
+ */
+//    public void fertilize(CropRow cropRow) {
+//
+//        for(int i = 0; i < )
+//    }
+
 
     @Override
     public void operate() {
