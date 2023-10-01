6e080bcf6e6545c9cd3b96ccb7123dcd87854388
Ki <kfchan93@gmail.com>
2023-10-01 12:22:52 -0400

    fixed croprow methods to public

--- src/main/java/org/FarmerFroilen/CropRow.java
+++ src/main/java/org/FarmerFroilen/CropRow.java
@@ -6,13 +6,29 @@ public class CropRow {
     public CropRow(){
         crops=new ArrayList<>();
     }
-    private void addCrop(Crop crop){
+
+    /**
+     * adds a crop at the end of the row
+     * @param crop
+     */
+    public void addCrop(Crop crop){
         crops.add(crop);
     }
-    private Crop getCropAt(int x){
+
+    /**
+     *
+     * @param x
+     * @return returns the crop at x index
+     */
+    public Crop getCropAt(int x){
         return crops.get(x);
     }
-    private void removeCropAt(int x){
+
+    /**
+     *
+     * @param x gives the index of the Crop wanted to be removed
+     */
+    public void removeCropAt(int x){
         crops.remove(x);
     }
 }
