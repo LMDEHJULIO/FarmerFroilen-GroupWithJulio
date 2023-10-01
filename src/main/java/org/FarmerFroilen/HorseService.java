d76df07f73ff283eeb49aa3334f8f55995f2cd2c
Julio Rodriguez <juliodevelops@gmail.com>
2023-10-01 13:34:17 -0400

    Add Horse files before trying global cors

---
+++ src/main/java/org/FarmerFroilen/HorseService.java
@@ -0,0 +1,30 @@
+package org.FarmerFroilen;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HorseService {
+
+    @Autowired
+
+    private HorseRepository horseRepository;
+
+    @Autowired
+
+    private MongoTemplate mongoTemplate;
+
+    public List<Horse> allHorsies(){
+        return horseRepository.findAll();
+    }
+
+    public Horse createHorsie(){
+        Horse horse = horseRepository.insert(new Horse());
+
+        return horse;
+    }
+}
