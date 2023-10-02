f26852f290fcd373e96ae77550afba4aba64f5ab
SpaceTilmar <21tilmaro@gmail.com>
2023-10-02 14:48:20 -0400

    Fixed Cropduster

--- src/main/java/org/FarmerFroilen/CropDuster.java
+++ src/main/java/org/FarmerFroilen/CropDuster.java
@@ -1,26 +1,35 @@
 package org.FarmerFroilen;
 import java.util.*;
 public class CropDuster extends Aircraft implements FarmVehicle {
-    // test error
-    public CropDuster(String name) {
-        super(name);
+    public CropDuster(String name, String noise, boolean rideable) {
+        super(name, noise, rideable);
     }
+    // test error
+
+
 /*
 Iterate through the cropRow arrayList and check if each crop hasBeenFertilized, if not then fertilize the crop.
- */
-  //  public void fertilize(CropRow cropRow) {
+ */  public void fertilize(CropRow cropRow) {
 
-      /*  for(int i = 0; i < cropRow.getCrops.size; i++){
+       for(int i = 0; i < cropRow.returnCrop().size(); i++){
             Crop currentCrop = cropRow.getCropAt(i);
-            if(currentCrop.getHasBeenFertilized != true){
+            if(!currentCrop.getHasBeenFertilized()){
                 currentCrop.setHasBeenFertilized(true);
             }
         }
     }
-*/
 
     @Override
     public void operate() {
 
+    }
+
+    @Override
+    public String toString() {
+        return "CropDuster{" +
+                "name='" + getName() + '\'' +
+                ", noise='" + getNoise() + '\'' +
+                ", rideable=" + isRideable() +
+                '}';
     }
 }
