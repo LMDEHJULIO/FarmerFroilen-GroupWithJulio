1b046559dc5c4011ef578fff5ec393db1d1b362d
Genbuos <swiftypython18@gmail.com>
2023-10-02 15:24:32 -0400

    add test case for pilot class

--- src/test/java/org/FarmerFroilen/PilotTest.java
+++ src/test/java/org/FarmerFroilen/PilotTest.java
@@ -3,5 +3,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class PilotTest {
 
+    @Test
+    void isPerson(){
+        var ace = new Pilot("Froilinda");
+
+        assertTrue(ace instanceof Person);
+    }
 
 }
