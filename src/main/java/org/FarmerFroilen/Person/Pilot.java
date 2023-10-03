f88bb90d33ec080c1972a2f3f4046d48ee119ae6
Genbuos <swiftypython18@gmail.com>
2023-10-03 13:42:27 -0400

    new methods in pilot

--- src/main/java/org/FarmerFroilen/Person/Pilot.java
+++ src/main/java/org/FarmerFroilen/Person/Pilot.java
@@ -1,14 +1,13 @@
 package org.FarmerFroilen.Person;
 
 import lombok.Data;
-import org.FarmerFroilen.Person.Pilot;
 import org.FarmerFroilen.Vehicle.Aircraft;
 import org.FarmerFroilen.Interface.Edible;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "pilot")
 @Data
-public class Pilot extends Person{
+public class Pilot extends Person {
     public Pilot(String name) {
         super(name);
     }
@@ -31,7 +30,19 @@ public class Pilot extends Person{
         System.out.println("I can fly");
     }
 
+    public boolean getOn(Aircraft aircraft){
+        if(aircraft.isFlying() || aircraft.hasPilot() == true){
+            return false;
+        }
+        return true;
+    }
 
+    public boolean getOff(Aircraft aircraft){
+        if( aircraft.hasPilot() == true && aircraft.isFlying() == false){
+            return true;
+        }
+        return false;
+    }
 
 
 }
