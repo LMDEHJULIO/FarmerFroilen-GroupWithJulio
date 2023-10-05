d9b00ef23e2be1f1eff3c4487ad683169c12ed2c
Julio Rodriguez <juliodevelops@gmail.com>
2023-10-05 10:41:07 -0400

    Add before each to farmertest

--- src/test/java/org/FarmerFroilen/FarmerTest.java
+++ src/test/java/org/FarmerFroilen/FarmerTest.java
@@ -5,37 +5,46 @@ import org.FarmerFroilen.Interface.Eater;
 import org.FarmerFroilen.Interface.Rider;
 import org.FarmerFroilen.Person.Farmer;
 import org.FarmerFroilen.Person.Person;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class FarmerTest {
+    Farmer farmer;
+
+    @BeforeEach
+
+    public void setup(){
+        farmer = new Farmer("Froiland");
+    }
 
     // checks if the farmer is a Eater
     @Test
     void isEater(){
-        var farmer = new Farmer("Mikaila");
         assertTrue(farmer instanceof Eater);
     }
     // checking the rider implementation
     @Test
     void isRider(){
-        var farmer = new Farmer("Foilian");
         assertTrue( farmer instanceof Rider);
     }
     // checking botanist implementation
     @Test
     void isBotanist(){
-        var farmer = new Farmer("Ki");
         assertTrue(farmer instanceof Botanist);
     }
 
     // checking the person inheritence
     @Test
     void isPerson(){
-        var farmer = new Farmer("Tanzir");
-
         assertTrue(farmer instanceof Person);
     }
+
+//    @Test
+
+//    void plant(){
+//        farmer.plant();
+//    }
 
 }
